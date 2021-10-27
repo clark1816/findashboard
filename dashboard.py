46128358aa0453145474e022827980526bfd0d5e
clark1816 <33646776+clark1816@users.noreply.github.com>
2021-10-27 17:12:05 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -17,10 +17,10 @@ auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_
 auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-connection = psycopg2.connect(host = config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS, port='5432')
-cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+#connection = psycopg2.connect(host = config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS, port='5432')
+#cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'wallstreetbets', 'stocktwits', 'pattern','company info'), 1)
+option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'stocktwits','company info'), 1)
 
 st.header(option)
 
@@ -94,89 +94,3 @@ if option == 'stocktwits':
         st.write(message['created_at'])
         st.write(message['body'])
 
-if option == 'wallstreetbets':
-    num_days = st.sidebar.slider('Number of days', 1, 30, 3)
-    
-    st.info('Number of times a ticker has been mentioned in wallstreet bets reddit page.')
-
-    cursor.execute("""
-        SELECT COUNT(*) AS num_mentions, symbol
-        FROM mention JOIN stock ON stock.id = mention.stock_id
-        WHERE date(dt) > current_date - interval '%s day'
-        GROUP BY stock_id, symbol   
-        HAVING COUNT(symbol) > 10
-        ORDER BY num_mentions DESC
-    """, (num_days,))
-
-    counts = cursor.fetchall()
-    for count in counts:
-        st.write(count)
-    
-    cursor.execute("""
-        SELECT symbol, message, url, dt
-        FROM mention JOIN stock ON stock.id = mention.stock_id
-        ORDER BY dt DESC
-        LIMIT 100
-    """)
-
-    mentions = cursor.fetchall()
-    for mention in mentions:
-        st.text(mention['dt'])
-        st.text(mention['symbol'])
-        st.text(mention['message'])
-        st.text(mention['url'])
-
-    rows = cursor.fetchall()
-
-    st.write(rows)
-
-# if option == 'pattern':
-#     pattern = st.sidebar.selectbox(
-#         "Which Pattern?",
-#         ("engulfing", "threebar")
-#     )
-
-#     if pattern == 'engulfing':
-#         cursor.execute("""
-#             SELECT * 
-#             FROM ( 
-#                 SELECT day, open, close, stock_id, symbol, 
-#                 LAG(close, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_close, 
-#                 LAG(open, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_open 
-#                 FROM daily_bars
-#                 JOIN stock ON stock.id = daily_bars.stock_id
-#             ) a 
-#             WHERE previous_close < previous_open AND close > previous_open AND open < previous_close
-#             AND day = '2021-10-17'
-#         """)
-
-#     if pattern == 'threebar':
-#         cursor.execute("""
-#             SELECT * 
-#             FROM ( 
-#                 SELECT day, close, volume, stock_id, symbol, 
-#                 LAG(close, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_close, 
-#                 LAG(volume, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_volume, 
-#                 LAG(close, 2) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_close, 
-#                 LAG(volume, 2) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_volume, 
-#                 LAG(close, 3) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_previous_close, 
-#                 LAG(volume, 3) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_previous_volume 
-#             FROM daily_bars 
-#             JOIN stock ON stock.id = daily_bars.stock_id) a 
-#             WHERE close > previous_previous_previous_close 
-#                 AND previous_close < previous_previous_close 
-#                 AND previous_close < previous_previous_previous_close 
-#                 AND volume > previous_volume 
-#                 AND previous_volume < previous_previous_volume 
-#                 AND previous_previous_volume < previous_previous_previous_volume 
-#                 AND day = '2021-10-17'
-#         """)
-
-#     rows = cursor.fetchall()
-
-#     for row in rows:
-#         st.image(f"https://finviz.com/chart.ashx?t={row['symbol']}")
-url = 'http://127.0.0.1:5000/'
-if option == 'pattern':
-    st.text('This option is not live it will be live once I get funding I have it set up but I dont have money for the software to make it live.')
-    webbrowser.open_new_tab(url)
