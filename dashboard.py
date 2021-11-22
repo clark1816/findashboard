31fe3181a14f2d8abe463ebb9466ff5f3d6ca309
clark1816 <33646776+clark1816@users.noreply.github.com>
2021-11-22 15:47:55 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -139,39 +139,7 @@ if option == 'nftdashboard':
 
 
     if endpoint == 'Events':
-        collection = st.sidebar.text_input("Collection")
-        asset_contract_address = st.sidebar.text_input("Contract Address")
-        token_id = st.sidebar.text_input("Token ID")
-        event_type = st.sidebar.selectbox("Event Type", ['offer_entered', 'cancelled', 'bid_withdrawn', 'transfer', 'approve'])
-        params = {}
-        if collection:
-            params['collection_slug'] = collection
-        if asset_contract_address:
-            params['asset_contract_address'] = asset_contract_address
-        if token_id:
-            params['token_id'] = token_id
-        if event_type:
-            params['event_type'] = event_type
-        
-        r = requests.get('https://api.opensea.io/api/v1/events', params=params)
-
-        #events = r.json()
-        event_list = []
-        for event in events['asset_events']:
-            if event_type == 'offer_entered':
-                if event['bid_amount']:
-                    bid_amount = Web3.fromWei(int(event['bid_amount']), 'ether')
-                if event['from_account']['user']:
-                    bidder = event['from_account']['user']['username']
-                else:
-                    bidder = event['from_account']['address']
-
-                event_list.append([event['created_date'], bidder, float(bid_amount), event['asset']['collection']['name'], event['asset']['token_id']])
-
-        df = pd.DataFrame(event_list, columns=['time', 'bidder', 'bid_amount', 'collection', 'token_id'])
-        st.write(df)
-        
-        st.write(events)
+        st.write('I accidentaly broke one of the rules of the OpenSea Events API so this feature is down till they unban RMUs IP address from the site')
 
     if endpoint == 'Assets':
         st.sidebar.header('Filters')
