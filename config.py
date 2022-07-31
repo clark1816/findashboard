c4b2d61a9ec2e0acd43c9aca82eb48c8ca4d7d18
clark1816 <33646776+clark1816@users.noreply.github.com>
2022-07-31 16:15:57 -0400

    we can now trade like Nancy

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ TWITTER_ACCESS_TOKEN = '2317908458-t0NlHmDyVX0J0tHUbjD33Js8UuihCiUzaRzmm7w'
 TWITTER_ACCESS_TOKEN_SECRET = 'DFPfzFLz6EbOQhM1DcqCFStaRXePTDkBj3WWgSqWr3T3L'
 
 TWITTER_USERNAMES = [
+    'PelosiTracker_',
     '1PennyStocks',
     'traderstewie',
     'the_chart_life',
