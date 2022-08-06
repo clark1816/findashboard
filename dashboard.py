8455cefe93127a8462e6dc708dc0f66d9a517c07
clark1816 <33646776+clark1816@users.noreply.github.com>
2022-08-06 15:03:41 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -26,6 +26,52 @@ option = st.sidebar.selectbox("Which Dashboard?", ('wallstreetbets','candle patt
 
 st.header(option)
 
+if option == 'candle pattern':
+    pattern = st.sidebar.selectbox(
+        "Which Pattern?",
+        ("engulfing", "threebar")
+    )
+
+    if pattern == 'engulfing':
+        cursor.execute("""
+            SELECT * 
+            FROM ( 
+                SELECT day, open, close, stock_id, symbol, 
+                LAG(close, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_close, 
+                LAG(open, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_open 
+                FROM daily_bars
+                JOIN stock ON stock.id = daily_bars.stock_id
+            ) a 
+            WHERE previous_close < previous_open AND close > previous_open AND open < previous_close
+            AND day = '2021-02-18'
+        """)
+
+    if pattern == 'threebar':
+        cursor.execute("""
+            SELECT * 
+            FROM ( 
+                SELECT day, close, volume, stock_id, symbol, 
+                LAG(close, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_close, 
+                LAG(volume, 1) OVER ( PARTITION BY stock_id ORDER BY day ) previous_volume, 
+                LAG(close, 2) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_close, 
+                LAG(volume, 2) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_volume, 
+                LAG(close, 3) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_previous_close, 
+                LAG(volume, 3) OVER ( PARTITION BY stock_id ORDER BY day ) previous_previous_previous_volume 
+            FROM daily_bars 
+            JOIN stock ON stock.id = daily_bars.stock_id) a 
+            WHERE close > previous_previous_previous_close 
+                AND previous_close < previous_previous_close 
+                AND previous_close < previous_previous_previous_close 
+                AND volume > previous_volume 
+                AND previous_volume < previous_previous_volume 
+                AND previous_previous_volume < previous_previous_previous_volume 
+                AND day = '2021-02-19'
+        """)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        st.image(f"https://finviz.com/chart.ashx?t={row['symbol']}")
 
 if option == 'twitter':
     for username in config.TWITTER_USERNAMES:
